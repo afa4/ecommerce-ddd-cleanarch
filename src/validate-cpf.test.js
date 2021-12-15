6c273c3594bd45e45708ffc960ac54df409550a4
Arthur Frade de Araújo <arthur.frade@loft.com.br>
2021-12-15 11:17:46 -0300

    initial commit

---
+++ src/validate-cpf.test.js
@@ -0,0 +1,52 @@
+const cpfValidator = require("./validate-cpf-after");
+
+test('should return false when input is null', () => {
+  const result = cpfValidator.validate(null);
+  expect(result).toBe(false);
+});
+
+test('should return false when input is undefined', () => {
+  const result = cpfValidator.validate(undefined);
+  expect(result).toBe(false);
+});
+
+test('should return false when input type is not string', () => {
+  const result = cpfValidator.validate(123);
+  expect(result).toBe(false);
+});
+
+test('should return false if input length is less than 11', () => {
+  const result = cpfValidator.validate("935.411.347-8");
+  expect(result).toBe(false);
+});
+
+test('should return false if input length is bigger than 14', () => {
+  const result = cpfValidator.validate("935.411.347-800");
+  expect(result).toBe(false);
+});
+
+test('should return false if input has illegal chars', () => {
+  const result = cpfValidator.validate("aaa.411.347-8a");
+  expect(result).toBe(false);
+});
+
+test.each([
+  '935.411.347-80',
+  '111.444.777-35',
+  '801.497.840-40',
+  '263.850.990-67'
+])('should return true if input is valid cpf with special chars', (cpf) => {
+  const result = cpfValidator.validate(cpf);
+  expect(result).toBe(true);
+});
+
+test.each([
+  '93541134780',
+  '11144477735',
+  '80149784040',
+  '26385099067'
+])('should return true if input is valid cpf', (cpf) => {
+  const result = cpfValidator.validate(cpf);
+  expect(result).toBe(true);
+});
+
