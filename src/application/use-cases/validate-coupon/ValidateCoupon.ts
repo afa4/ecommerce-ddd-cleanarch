bfbd701d35262f65ac769fbb9718afeb24cc6a06
Arthur Frade de Araújo <arthur.frade@loft.com.br>
2021-12-30 19:01:26 -0300

    integration test script

--- src/application/use-cases/validate-coupon/ValidateCoupon.ts
+++ src/application/use-cases/validate-coupon/ValidateCoupon.ts
@@ -5,8 +5,7 @@ export default class ValidateCoupon implements UseCase<string, boolean> {
 
   constructor(private readonly couponRepository: CouponRepository) {}
 
-  async execute(couponCode: string): Promise<boolean>
-  {
+  async execute(couponCode: string): Promise<boolean> {
     const coupon = await this.couponRepository.findById(couponCode);
     if(!coupon) return Promise.resolve(false);
     return Promise.resolve(coupon.isValid());
--- src/domain/repository/ItemRepository.ts
+++ src/domain/repository/ItemRepository.ts
@@ -2,5 +2,5 @@ import Item from '../entity/Item';
 
 export default interface ItemRepository {
   findByIdIn(ids: number[]): Promise<Item[]>;
-  getItemsMappedByIdWhereIdIn(ids: number[]): Promise<{[itemId: number]: Item}>
+  getItemsMappedByIdWhereIdIn(ids: number[]): Promise<{[itemId: number]: Item}>;
 }
