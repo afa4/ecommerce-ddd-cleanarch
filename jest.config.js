552dd58c10736cbb590c46dc44ce02551b66da47
Arthur Frade de Araújo <arthur.frade@loft.com.br>
2022-04-14 14:43:39 -0300

    feat: adding link to test speed github thread

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  maxWorkers: 1, // workaround to improve test speed
+  maxWorkers: 1, // workaround to improve test speed: https://github.com/kulshekhar/ts-jest/issues/259#issuecomment-769774111
 };
